18c264c5720a8299836f514d67c7be8191ea1dd2
MichalDziedzic <michaldziedzic8@gmail.com>
2021-09-23 20:43:22 +0200

    add styled component , display data in JobsWrapper

--- app/src/component/JobItem.js
+++ app/src/component/JobItem.js
@@ -6,8 +6,8 @@ const JobItem = (props) => {
           <div className="job-item">
                <img src="#" alt="Logo" />
                <div className="job-item-desc">
-                    <p className="company-name"></p>
-                    <h3 className="job-title">Frontend dev</h3>
+                    <p className="company-name">{props.company}</p>
+                    <h3 className="job-title">{props.position}</h3>
                </div>
                <div className="job-hash-tags">
                     <TechJobItem />
